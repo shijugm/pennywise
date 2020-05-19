4e48ee4895c92f6a3083fbaec2d4f1d46cf3d503
Shiju <shiju@Shijus-iMac.local>
2020-05-19 21:47:57 +1200

    sidebar menu added - v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css'; 
+import './css/sidebar.css'; 
+import SideBar from './sidebar';  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+
+      <div id="page-wrap">
+        <h1>Pennywise</h1>
+        <h2>Check out our first page!</h2>
+      </div>
     </div>
   );
 }
 
 export default App;
+  
